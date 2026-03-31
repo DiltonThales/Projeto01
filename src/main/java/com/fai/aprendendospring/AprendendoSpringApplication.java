c1d8663f3d87565cac444d76cb3d6c030fdaded1
dilton <diltonthales.dt@gmail.com>
2026-03-30 23:26:10 -0300

    feat: criacao das tabelas usuario, teléfono e usuario

--- src/main/java/com/fai/aprendendospring/AprendendoSpringApplication.java
+++ src/main/java/com/fai/aprendendospring/AprendendoSpringApplication.java
@@ -10,10 +10,10 @@ public class AprendendoSpringApplication {
 	public static void main(String[] args)  {
 		SpringApplication.run(AprendendoSpringApplication.class, args);
 
-		Usuario usuario = new Usuario("João","joao@gmail.com","1584848");
-		System.out.println(usuario.getNome());
-		usuario.setNome("Abigail");
-		System.out.println(usuario.getNome());
+//		Usuario usuario = new Usuario("João","joao@gmail.com","1584848");
+//		System.out.println(usuario.getNome());
+//		usuario.setNome("Abigail");
+//		System.out.println(usuario.getNome());
 
 	}
 
