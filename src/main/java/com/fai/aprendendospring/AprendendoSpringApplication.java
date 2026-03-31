154c13d99b0837a8f2d8ba06e5b9436b895f87bb
dilton <diltonthales.dt@gmail.com>
2026-03-30 23:04:15 -0300

    feat: adicao do lombok e esqueleto do codigo

--- src/main/java/com/fai/aprendendospring/AprendendoSpringApplication.java
+++ src/main/java/com/fai/aprendendospring/AprendendoSpringApplication.java
@@ -1,13 +1,20 @@
 package com.fai.aprendendospring;
 
+import com.fai.aprendendospring.infrastructure.entity.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AprendendoSpringApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(AprendendoSpringApplication.class, args);
+
+		Usuario usuario = new Usuario("João","joao@gmail.com","1584848");
+		System.out.println(usuario.getNome());
+		usuario.setNome("Abigail");
+		System.out.println(usuario.getNome());
+
 	}
 
 }
