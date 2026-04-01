0660dee9772a5434f459f01a8ae08f7a3d32b856
dilton <diltonthales.dt@gmail.com>
2026-04-01 20:18:48 -0300

    feat: adicao usuario dtp e ajuste nos usuarios controller e repository

--- src/main/java/com/fai/aprendendospring/controller/UsuarioController.java
+++ src/main/java/com/fai/aprendendospring/controller/UsuarioController.java
@@ -2,9 +2,14 @@ package com.fai.aprendendospring.controller;
 
 
 import com.fai.aprendendospring.business.UsuarioService;
+import com.fai.aprendendospring.controller.dto.UsuarioDTO;
 import com.fai.aprendendospring.infrastructure.entity.Usuario;
+import com.fai.aprendendospring.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +22,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
 
     @PostMapping
     public ResponseEntity<Usuario> salvaUsuario(@RequestBody Usuario usuario){
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuario));
 
+    }
+
+    @PostMapping("/usuario/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
+                        usuarioDTO.getSenha())
+        );
+        return jwtUtil.generateToken(authentication.getName());
     }
 
 
